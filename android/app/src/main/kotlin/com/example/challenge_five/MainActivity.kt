87e99e71ce615ab2b47a92faabe3b28e16578de0
Si Thu <hhtz12450@gmail.com>
2020-12-21 23:40:43 +0630

    version1

---
+++ android/app/src/main/kotlin/com/example/challenge_five/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.challenge_five
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Si Thu Shein'),
+          centerTitle: true,
+        ),
+        body: MagicBallApp(),
+      ),
+    );
+  }
+}
+
+class MagicBallApp extends StatefulWidget {
+  @override
+  _MagicBallAppState createState() => _MagicBallAppState();
+}
+
+class _MagicBallAppState extends State<MagicBallApp> {
+  var magicballresult = 1;
+  void magicballfunc() {
+    setState(() {
+      magicballresult = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  magicballfunc();
+                },
+                child: Image.asset('images/ans$magicballresult.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
